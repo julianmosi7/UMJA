73257eb1009c9b39e0e3229492d2e2c323fa3026
julianmosi <moshammer.ju@gmail.com>
2019-10-20 19:05:48 +0200

    draganddrop and filechooser done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,59 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.stage.FileChooser;
+
+import java.beans.XMLDecoder;
+import java.io.*;
+import java.util.List;
+
 public class Controller {
+
+    public AnchorPane draganddrop;
+    List<File> files;
+    final FileChooser fileChooser = new FileChooser();
+    private File selectedFile;
+
+
+    public void onDragOver(DragEvent dragEvent) {
+        draganddrop.setStyle("-fx-background-color: grey;");
+        if(dragEvent.getDragboard().hasFiles()){
+            dragEvent.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
+    public void onDragDropped(DragEvent dragEvent) {
+        files = dragEvent.getDragboard().getFiles();
+    }
+
+    public void onMouseClicked(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open file");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("GRAPHML", "*.graphml"));
+        selectedFile = fileChooser.showOpenDialog(draganddrop.getScene().getWindow());
+        if(selectedFile != null){
+            open(selectedFile);
+        }
+    }
+
+    private void open(File file){
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            while(br.ready()){
+                String line = br.readLine();
+                //System.out.println(line);
+                String[] teile = line.split(";");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
