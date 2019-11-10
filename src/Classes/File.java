1d72b5a81f5e02156f3a4df93111c9d735f580e1
Matthias Wetzlmayer <mwetzl@gmail.com>
2019-11-10 14:51:40 +0100

    String pack in class File added

--- src/Classes/File.java
+++ src/Classes/File.java
@@ -7,16 +7,18 @@ public class File {
     FileType fileType;
     String name;
     File parent;
+    String pack;
     List<File> implement;
     List<Variable> attributes;
     List<Method> methodes;
 
-    public File(FileType fileType, String name, File parent, List<File> implement, List<Variable> attributes, List<Method> methodes) {
+    public File(FileType fileType, String name, File parent, List<File> implement, List<Variable> attributes, List<Method> methodes, String pack) {
         this.fileType = fileType;
         this.name = name;
         this.parent = parent;
         this.implement = implement;
         this.attributes = attributes;
         this.methodes = methodes;
+        this.pack=pack;
     }
 }
--- src/Classes/TestData.java
+++ src/Classes/TestData.java
@@ -11,14 +11,14 @@ public class TestData {
                 new Variable(true,true,"double", "someConst", "3.14")
         ), Arrays.asList(
                 new Method(true, true, "int", new Variable[]{new Variable(true, null, "int", "someParam", null)}, "method1")
-        ));
+        ), "testPack");
 
         File aClass1 = new File(FileType.Class, "aClass1", null, Arrays.asList(), Arrays.asList(
                 new Variable(false, false, "String", "Var1", "hello world"),
                 new Variable(false, false, "int", "twoVar", "2")
         ), Arrays.asList(
                 new Method(false, false, "boolean", null, "Class1Method")
-        ));
+        ), "testPack");
 
         File aClass2 = new File(FileType.Class, "aClass2", aClass1, Arrays.asList(aInterface), Arrays.asList(
                 new Variable(false, true, "String", "stringVar", "...g"),
@@ -28,12 +28,12 @@ public class TestData {
                         new Variable(true, null, "String", "stringPar",null),
                         new Variable(true, null, "int", "intPar",null)
                 }, "Class1Method")
-        ));
+        ), "testPack2");
 
         File aEnum = new File(FileType.Enum, "anEnum", null, Arrays.asList(), Arrays.asList(
                 new Variable(null, null, "Enum", "FIRST", null),
                 new Variable(null, null, "Enum", "SECOND", null)
-        ), Arrays.asList());
+        ), Arrays.asList(), "testPack2");
 
         return Arrays.asList(aInterface, aClass1, aClass2, aEnum);
     }
