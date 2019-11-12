2c5d6659d6fa6d8a65277b3f162e4f48ea902cd3
Jaki178 <elias.baran.bauer@gmx.at>
2019-11-12 16:01:07 +0100

    Parse HTML finish

--- src/sample/HTMLParser.java
+++ src/sample/HTMLParser.java
@@ -12,58 +12,124 @@ public class HTMLParser {
     //u BEDEUTET static
     //: BEDEUTET TYP
     //NAME IN UPPERCASE BEDEUTET final
+
+    // static - SCANNER : Scanner
     private String text = "&lt;html&gt;\n" +
-            "- &lt;u&gt;SCANNER : Scanner&lt;/u&gt;&lt;br&gt;\n" + // static - SCANNER : Scanner
+            "- &lt;u&gt;SCANNER : Scanner&lt;/u&gt;&lt;br&gt;\n" +
             "- &lt;u&gt;STORE : Store\n" +
             "&lt;/u&gt;&lt;br&gt;\n" +
             "- &lt;u&gt;user : User\n" +
             "&lt;/u&gt;\n" +
             "&lt;/html&gt;";
+    String text2 = "- name : String\n" +
+            "- price : String\n" +
+            "- published : String";
+
+    String text3 = "+ getGames() : Game[]\n" +
+            "+ addGameToUserWishlist(user2 : User2, gameName2 : String2) : void\n" +
+            "+ addGameToUserLibrary(user : User, gameName : String) : void";
 
     private boolean pub;
+    private boolean isMethod;
     private boolean isStatic = false;
-    private String finishedString;
+    private String finishedString = "";
+    private String clipString = "";
 
-    public String htmlToString(String string2) {
-        String[] strings = text.split("\\n");
+    public String htmlToString(String stringtext) {
+        String[] strings = stringtext.split("\\n");
         for (String string : strings) {
             String[] parts = string.split(";");
             for (int i = 0; i < parts.length; i++) {
                 if (parts[i].equals("u&gt")) {
                     isStatic = true;
+                    finishedString += "static ";
                 } else {
                     isStatic = false;
                 }
 
                 if (parts[i].equals("- &lt")) {
+                    finishedString += " - ";
                     pub = false;
-                } else {
+                } if(parts[i].equals("+ &lt")) {
+                    finishedString += " + ";
+                    pub = true;
+                } if(parts[i].equals("# &lt")) {
+                    finishedString += " # ";
                     pub = true;
                 }
 
-                if (parts[i].contains(" : ")) {
-                    String[] infoForClassOrVariable = parts[i].split(" : ");
-                    for (String inf : infoForClassOrVariable) {
-                        if (inf.contains("&lt")) {
-                            inf.replace("&lt", "");
-                            inf.replace("&gt", "");
-                        }
-                        if (inf.contains("(")) {
-                            String clip = inf.substring(inf.indexOf("("), inf.indexOf(")"));
-                            if (clip.length() > 3) {
-                                if (clip.contains(":")) {
-                                    String[] clipParameter = clip.split(" : ");
-                                    for (int j = 0; j < clipParameter.length; j++) { //String parameter : clipParameter){
-                                        //string fertigstellen
+                if (parts[i].contains("(")) {
+                    String clip = parts[i].substring(parts[i].indexOf("(")+1, parts[i].indexOf(")"));
+                    if (clip.length() > 3) {
+                        if (clip.contains(",")) {
+                            String[] clipValues = clip.split(",");
+                            for(int j = 0; j < clipValues.length; j++){
+                                    String[] clipParameter = clipValues[j].split(":");
+                                    for(int k = 0; k < clipParameter.length; k++){
+                                        if(k == clipParameter.length-1){
+                                            if(j != clipValues.length-1){
+                                                clipString += clipParameter[k] + ", ";
+                                            } else {
+                                                clipString += clipParameter[k];
+                                            }
+                                        } else {
+                                            clipString += clipParameter[k] + " : ";
+                                        }
                                     }
-                                }
+
+
                             }
+               /*             for (String clipinfo : clipValues) {
+                                if (clipinfo.contains(":")) {
+                                    String[] clipParameter = clipinfo.split(":");
+                                    clipString += clipParameter[0] + " : " + clipParameter[1];
+                                }
+                            } */
                         }
                     }
                 }
 
-            }
-        }
-        return "";
+                if(parts[i].contains("(")){
+                    parts[i] = parts[i].replace(parts[i].substring(parts[i].indexOf("("), parts[i].indexOf(")")+1), "");
+                    isMethod = true;
+                }
+                        if (parts[i].contains(" : ")) {
+                            String[] infoForClassOrVariable = parts[i].split(" : ");
+                    /*for (String inf : infoForClassOrVariable) {
+                        if (inf.contains("&lt")) {
+                            inf.replace("&lt", "");
+                            inf.replace("&gt", "");
+                        }
+                    } */
+                            for (int j = 0; j < infoForClassOrVariable.length; j++) {
+                                if (infoForClassOrVariable[j].contains("&lt")) {
+                                    infoForClassOrVariable[j] = infoForClassOrVariable[j].replace("&lt", "");
+                                    infoForClassOrVariable[j] = infoForClassOrVariable[j].replace("&gt", "");
+                                }
+                            }
+                            if(isMethod){
+                                finishedString += infoForClassOrVariable[0] + "(" + clipString + ") : " + infoForClassOrVariable[1] + " ";
+                            }
+                            else{
+                                finishedString += infoForClassOrVariable[0] + clipString + " : " + infoForClassOrVariable[1] + " ";
+                            }
+                            clipString = "";
+
+                  /*          for (String inf : infoForClassOrVariable) {
+                                if (inf.contains("(")) {
+                                    clip = inf.substring(inf.indexOf("("), inf.indexOf(")") + 1); //Funktioniert wegen dem split(":"); noch nicht. Der Fehler besteht weil er keine ")" findet.
+                                    if (clip.length() > 3) {
+                                        if (clip.contains(":")) {
+                                            String[] clipParameter = clip.split(" : ");
+                                            //for (int j = 0; j < clipParameter.length; j++) { //String parameter : clipParameter){
+                                            finishedString += clipParameter[0] + " : " + clipParameter[1];
+                                            //}
+                                        }*/
+                        }
+                    }
+                }
+                finishedString += "\n";
+        System.out.println(finishedString);
+        return finishedString;
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.HTML;
+
 public class Main extends Application {
 
     @Override
@@ -19,6 +21,13 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        HTMLParser parser = new HTMLParser();
+        parser.htmlToString("&lt;html&gt;\n" +
+                "- &lt;u&gt;SCANNER : Scanner&lt;/u&gt;&lt;br&gt;\n" +
+                "- &lt;u&gt;STORE : Store\n" +
+                "&lt;/u&gt;&lt;br&gt;\n" +
+                "- &lt;u&gt;user : User\n" +
+                "&lt;/u&gt;\n" +
+                "&lt;/html&gt;");
     }
 }
