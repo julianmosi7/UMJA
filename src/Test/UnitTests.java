15c08f68260468a554be430944af1fc9718a83e5
mathi <mathias-achleitner@gmx.at>
2019-11-30 16:15:26 +0100

    fixed some bugs in the tests

--- src/Test/UnitTests.java
+++ src/Test/UnitTests.java
@@ -46,14 +46,14 @@ public class UnitTests {
     public void testEnum(){
         System.out.println("testEnum ->");
         String java = getJava("..\\Package_1\\Enum");
-        assertTrue(java.matches("/(package\\s+)(Test\\.JavaTest\\.Package_1)( *;)\\s*public\\s*enum\\s*Enum\\s*{\\s*PROP_1\\s*,\\s*PROP_2\\s*,\\s*PROP_3\\s*,\\s*PROP_4\\s*}\\s*/"));
+        assertTrue(java.matches("/(package\\s+)(Test\\.JavaTest\\.Package_1)( *;)\\s*public\\s*enum\\s*Enum\\s*\\{\\s*PROP_1\\s*,\\s*PROP_2\\s*,\\s*PROP_3\\s*,\\s*PROP_4\\s*\\}\\s*/"));
     }
 
     @Test
     public void testInterface(){
         System.out.println("testEnum ->");
         String java = getJava("..\\Package_1\\Interface_1");
-        assertTrue(java.matches("/(\\s*package)(\\s*Test\\.JavaTest\\.Package_1)(\\s*;)(\\s*public)(\\s*interface)(\\s*Interface_1)(\\s*{(\\s*int\\s*constVar\\s*=\\s*2\\s*;)(\\s*public\\s*Double\\s*Method\\(\\)\\s*;)(\\s*}))/"));
+        assertTrue(java.matches("/(\\s*package)(\\s*Test\\.JavaTest\\.Package_1)(\\s*;)(\\s*public)(\\s*interface)(\\s*Interface_1)(\\s*\\{(\\s*int\\s*constVar\\s*=\\s*2\\s*;)(\\s*public\\s*Double\\s*Method\\(\\)\\s*;)(\\s*\\}))/"));
     }
 
     public String getJava(String file){
